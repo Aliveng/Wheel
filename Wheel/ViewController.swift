6bd4248deccc3209a4f498232edee5019065ca03
Aliveng <Aliveng@yandex.ru>
2021-04-16 14:09:10 +0300

    Initial commit

---
+++ Wheel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wheel
+//
+//  Created by Татьяна Севостьянова on 16.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
