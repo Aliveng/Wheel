115060355a5d495eb9e8e5af4eb6d53fb540bfe7
Aliveng <Aliveng@yandex.ru>
2021-04-20 11:36:52 +0300

    Почистила - FortuneWheelSlice

--- Wheel/FortuneWheelSlice.swift
+++ Wheel/FortuneWheelSlice.swift
@@ -14,7 +14,7 @@ class FortuneWheelSlice : CALayer {
     private var startAngle : Radians! // Угол начала сектора
     private var sectorAngle : Radians = -1 // Общий угол, который охватывает сектор
     private var slice : Slice! // Объект-сектор, содержащий данные сектора
-  
+    
     init(frame : CGRect , startAngle : Radians , sectorAngle : Radians , slice : Slice) {
         super.init()
         self.startAngle = startAngle // Это угол, под которым начинается сектор
@@ -35,65 +35,47 @@ class FortuneWheelSlice : CALayer {
         
         let radius = self.frame.width/2 - self.slice.borderWidth // Радиус колеса
         let center = CGPoint.init(x: self.frame.width/2, y: self.frame.height/2) // Расположение центра колеса
-        let image = self.slice.image.rotateImage(angle: self.startAngle)! // Поворот изображения
-        let lineLegth = CGFloat((2 * radius * sin(self.sectorAngle/2))) // Длина третьей линии в равнобедренном треугольнике. Расчитана формулой длинны хорды
-        let s = (radius + radius + lineLegth)/2 // Половина периметра - используется для расчета размера
+        let image = self.slice.image.rotateImage(angle: self.startAngle)! // !!! Поворот изображения
+        let lineLegth = CGFloat((2 * radius * sin(self.sectorAngle/2))) // Длина третьей линии в равнобедренном треугольнике - секторе. Расчитана формулой длинны хорды
+        let s = (radius + radius + lineLegth)/2 // Половина периметра - используется для расчета размера дуги сектора
         
-//     #4
-/**size calcutations based on Incenter radius for isosceles triange formula. increased the size by 1.5 instead of 2(as the calculation gives radius and we need diameter) to adjust the image properly inside the slice.*/
+        // !!! Для Изображения
+        // Вычисление размеров на основе радиуса для равнобедренного треугольника. Изменен размер на 1,5 вместо 2(так как расчет дает радиус и нам нужен диаметр), чтобы правильно настроить изображение внутри сектора
         let inCenterDiameter = ((s * (s - radius) * (s - radius) * (s - lineLegth)).squareRoot()/s) * 1.50
+        var size : CGFloat = 0 // !!! Размер квадрата изображения
+        size = self.sectorAngle == Degree(180).toRadians() ? radius/2 : self.sectorAngle == Degree(120).toRadians() ?  radius/1.9 : self.sectorAngle == Degree(90).toRadians() ? radius/1.9 : inCenterDiameter // !!? Для изображения. Размер для 180, 120 и 90 градусов регулируется вручную, чтобы правильно использовать пространство
+        size -= self.slice.borderWidth * 3 // !!! Уменьшение ширины границ линий по размеру квадрата изображения
         
-        //    #5
-        /**The size of the image Square*/
-        var size : CGFloat = 0 // Размер квадрата изображения
+        let height = 2 * (1 - cos(self.sectorAngle/2)) // Зазор между хордой и окружностью круга в центре сектора
         
-        /**Size for 180,120 adn 90 degrees is adjsted manually to properly uitlize the space*/
-        size = self.sectorAngle == Degree(180).toRadians() ? radius/2 : self.sectorAngle == Degree(120).toRadians() ?  radius/1.9 : self.sectorAngle == Degree(90).toRadians() ? radius/1.9 : inCenterDiameter
-        
-        /**Reducing the border width of the lines.in size*/
-        size -= self.slice.borderWidth * 3
-        
-        
-        /**Gap between chord and the circumference of the circle at the center of the sector.*/
-        let height = 2 * (1 - cos(self.sectorAngle/2))
-        
-        //      #6
-        /**X position of the Incenter of a isosceles triangle.Moved outside a bit to remove the Overlay of image over line.*/
+        // !!! X положение центра равнобедренного треугольника. Немного отодвинуто наружу, чтобы убрать наложение изображения на линию
         let xIncenter = ((radius * radius) + ((radius * cos(self.sectorAngle)) * radius))/(radius + radius + lineLegth) + (size * 0.07)
         
-        //      #7
-        /**Y position of the Incenter of a isosceles triangle*/
+        // !!! Y положение центра равнобедренного треугольника
         let yIncenter = ((radius * sin(self.sectorAngle)) * radius)/(radius + radius + lineLegth)
         
-        //      #8
-        //Center alignment of image.180,120 and 90 degrees positions are adjusted manually
-        
+        // !!! Выравнивание изображения по центру. положения 180,120 и 90 градусов регулируются вручную
         let xPosition : CGFloat = self.sectorAngle == Degree(180).toRadians() ? (-size/2) : self.sectorAngle == Degree(120).toRadians() ? (radius/2.7 - size/2) : self.sectorAngle == Degree(90).toRadians() ? (radius/2.4 - size/2) : ((xIncenter - size/2) + height)
-        
         let yPosition : CGFloat = self.sectorAngle == Degree(180).toRadians() ? size/1.6 : self.sectorAngle == Degree(120).toRadians() ? (radius/2 - size/2) : self.sectorAngle == Degree(90).toRadians() ? (radius/2.4 - size/2) : (yIncenter - size/2)
         
         
-        //Drawing the slice
-        
+        // Отрисовка сектора
         UIGraphicsPushContext(context)
         
-        // #1
         let path = UIBezierPath.init()
         path.lineWidth = self.slice.borderWidth
         path.move(to: center)
         path.addArc(withCenter: center, radius: radius, startAngle: self.startAngle, endAngle: self.startAngle + self.sectorAngle, clockwise: true)
         path.close()
-        //applies the slice color
+        // Заполняет цветом сектор
         self.slice.color.setFill()
         path.fill()
-        //Applies border color
+        // Заполняет цветом контур - обводку
         self.slice.borderColour.setStroke()
         path.stroke()
         UIGraphicsPopContext()
         
-        //  #9
-        //Image draw
-        
+        // !!! Рисунок изображения
         context.saveGState()
         context.translateBy(x: center.x, y: center.y)
         context.rotate(by: self.startAngle)
